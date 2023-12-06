5a0439ab1539afa2757803498c1e461f0688db97
Darks00le <101908410+Darks00le@users.noreply.github.com>
2023-12-06 17:56:28 +0000

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,7 +4,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $userInput = $_POST["userInput"];
 
     // Store the user input in a text file
-    $file = fopen("mrkeally/submissions.txt", "a"); // Open the file in append mode
+    $file = fopen("submissions.txt", "a"); // Open the file in append mode
 
     fwrite($file, $userInput . PHP_EOL);  // Write the user input to the file with a newline
     fclose($file);  // Close the file
